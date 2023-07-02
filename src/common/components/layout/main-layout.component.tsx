48a889e4f01a70aca7fad987be67fa99401dd5df
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-07-02 21:42:10 +0330

    style: add max width to app

--- src/common/components/layout/main-layout.component.tsx
+++ src/common/components/layout/main-layout.component.tsx
@@ -2,7 +2,7 @@ import { MainLayoutProperties } from "./main-layout-properties.interface";
 
 export const MainLayout = ({ children }: MainLayoutProperties) => {
   return (
-    <div className="container max-w-7xl min-w-mobile mx-auto p-10">
+    <div className="container max-w-7xl min-w-mobile mx-auto p-10 w-app">
       <main>{children}</main>
     </div>
   );
