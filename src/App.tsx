c10b95962ec250465b08de604bb1663f2e4e1488
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-07-02 19:10:52 +0330

    chore: reformat files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "common/utils/react-query-client";
-import { MainLayout } from "common/components";
+import { queryClient } from "./common/utils/react-query-client";
+import { MainLayout } from "./common/components";
 
 function App() {
   return (
