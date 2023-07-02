6beae6dc9d819594396b957f1a1e437be9585bad
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-07-02 20:12:39 +0330

    chore: change root route to contact module

--- src/common/utils/router.tsx
+++ src/common/utils/router.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import { ContactsModule } from "../../modules/contacts/contacts.module";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
-    element: <div>Contacts</div>,
+    element: <ContactsModule />,
   },
   {
     path: "/contact/:id",
