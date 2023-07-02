756b20ba437f4488be6e5fa8fd55ad95b94c7df7
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-07-02 19:40:46 +0330

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./common/utils/react-query-client";
 import { MainLayout } from "./common/components";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./common/utils/router";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainLayout>{/* TODO: add router */}</MainLayout>
+      <MainLayout>
+        <RouterProvider router={routes} />
+      </MainLayout>
     </QueryClientProvider>
   );
 }
