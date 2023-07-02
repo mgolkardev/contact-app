a87ee297339a142e47becfd89d81c861a8498ec7
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-07-02 20:14:05 +0330

    fix: change contact API address to passenger and add result set for the API

--- src/modules/contacts/apis/contacts.api.ts
+++ src/modules/contacts/apis/contacts.api.ts
@@ -1,20 +1,24 @@
-import { axiosClient } from "common/utils/axios-client";
 import { CONTACTS_PER_PAGE } from "./constants/contacts.constant";
 import { ContactDto } from "./dtos/contact.dto";
+import { ResultSet } from "../../../common/interfaces/result-set.interface";
+import { axiosClient } from "../../../common/utils/axios-client";
 
 export const getContacts = async (page: number, query?: string) => {
-  const { data } = await axiosClient.get<ContactDto[]>(`/contacts`, {
-    params: {
-      limit: CONTACTS_PER_PAGE,
-      skip: page * CONTACTS_PER_PAGE,
-      where: query
-        ? {
-            first_name: { contains: query },
-            last_name: { contains: query },
-            phone: { contains: query },
-          }
-        : undefined,
-    },
-  });
+  const { data } = await axiosClient.get<ResultSet<ContactDto[]>>(
+    `/passenger`,
+    {
+      params: {
+        limit: CONTACTS_PER_PAGE,
+        skip: page * CONTACTS_PER_PAGE,
+        where: query
+          ? JSON.stringify({
+              first_name: { contains: query },
+              // last_name: { contains: query },
+              // phone: { contains: query },
+            })
+          : undefined,
+      },
+    }
+  );
   return data;
 };
