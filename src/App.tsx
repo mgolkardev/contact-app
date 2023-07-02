59ddfba71dcff712ee767a236211f678bec3c69b
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-07-02 15:56:13 +0330

    refactor: change main app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "common/utils/react-query-client";
+import { MainLayout } from "common/components";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MainLayout>{/* TODO: add router */}</MainLayout>
     </QueryClientProvider>
   );
 }
--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./link/link.component";
 export * from "./grid/grid.component";
+export * from "./layout/main-layout.component";
